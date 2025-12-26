1d8c53ef71edac7d427b941c2c1c1d3738d41a1f
joe Ferreira <72210317+Hoso4rakida@users.noreply.github.com>
2025-12-26 12:33:44 +0000

    Preparação do java e do react no replit

---
+++ src/main/java/com/CrisArchict/SiteVendas/SiteVendasApplication.java
@@ -0,0 +1,13 @@
+package com.CrisArchict.SiteVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CrisArchict/SiteVendas/SiteVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CrisArchict.SiteVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
